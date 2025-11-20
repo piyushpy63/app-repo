adb19e0117f9fe7971183f48f7bfdb12a92d5974
piyush lale <96431711+piyushpy63@users.noreply.github.com>
2025-11-20 11:13:41 +0530

    Update hello message in app to test workflow

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-    return "hello from gitops cicd pipeline"
+    return "Hello from changed gitops pipeline to test workflow"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
