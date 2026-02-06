685eb51b2d946bcbb2aa3250d0c72b1bb001d360
piyush lale <96431711+piyushpy63@users.noreply.github.com>
2026-02-06 12:08:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-    return "Hello from changed gitops pipeline to test workflow"
+    return "Hello i changed the hello, can you see it"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
